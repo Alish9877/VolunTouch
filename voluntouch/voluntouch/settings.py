6554fed20e2094783b83bc83336d74cead63796f
alish9877 <alshaikhali302@gmail.com>
2025-01-15 13:03:45 +0300

    start

--- voluntouch/voluntouch/settings.py
+++ voluntouch/voluntouch/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'voluntouch.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'volunteer',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'PORT': 5432
     }
 }
 
