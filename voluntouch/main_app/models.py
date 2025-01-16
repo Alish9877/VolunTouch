32c5efe3441f84b9bd43de6eeb44bde1055a6c92
alish9877 <alshaikhali302@gmail.com>
2025-01-16 11:02:59 +0300

    app model

--- voluntouch/main_app/models.py
+++ voluntouch/main_app/models.py
@@ -2,3 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Application(models.Model):
+    AppDate = models.DateField()
+    Status = models.CharField(max_length=20)
