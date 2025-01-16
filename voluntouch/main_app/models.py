3f4da58dcb5c913f9c02521fe1e6859a32ef84da
alish9877 <alshaikhali302@gmail.com>
2025-01-16 11:49:10 +0300

    add app model - fix somethings

--- voluntouch/main_app/models.py
+++ voluntouch/main_app/models.py
@@ -2,10 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Organization(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    contactEmail = models.EmailField(max_length=100)
+    contactPhone = models.IntegerField()
+
 class Opportunity(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    organization = models.CharField(max_length=255)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
     location = models.CharField(max_length=255)
     start_date = models.DateField()
     end_date = models.DateField()
@@ -14,13 +21,7 @@ class Opportunity(models.Model):
     def __str__(self):
         return self.title
     
-class Organization(models.Model):
-    name = models.CharField(max_length=100)
-    location = models.CharField(max_length=100)
-    description = models.TextField(max_length=250)
-    contactEmail = models.CharField(max_length=100)
-    contactPhone = models.IntegerField()
-    
 class Application(models.Model):
     AppDate = models.DateField()
     Status = models.CharField(max_length=20)
+    opportunity = models.ForeignKey(Opportunity, on_delete=models.CASCADE)
