d9ccf01c18aa4712098ae3206e2ef620762774cf
alish9877 <alshaikhali302@gmail.com>
2025-01-19 10:01:41 +0300

    user done

--- voluntouch/main_app/admin.py
+++ voluntouch/main_app/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import Organization
-from .models import Opportunity
+from .models import Opportunity , Application , Profile
 
 # Register your models here.
 admin.site.register(Opportunity)
 admin.site.register(Organization)
+admin.site.register(Application)
+admin.site.register(Profile)
--- voluntouch/main_app/urls.py
+++ voluntouch/main_app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-        path('', views.home, name='home'),
+    path('', views.home, name='home'),
     path('accounts/signup/', views.signup, name='signup'),
-    # path('accounts/login/', views.login, name='login')
+    # path('accounts/login/', views.login, name='login'),
     path('about/', views.about, name='about'),
     path('opportunities/', views.opportunity_list, name='opportunity_list'),
     path('organizations/', views.organization_index, name="index"),
